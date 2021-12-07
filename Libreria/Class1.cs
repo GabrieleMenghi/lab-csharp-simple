0a2e930d25c2a8004fe96ec091b8bdc6674c4c54
Gabriele Menghi <gabriele.menghi@studio.unibo.it>
2021-12-07 16:11:00 +0100

    Added a test library

---
+++ Libreria/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
+{
+    public class Class1
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
